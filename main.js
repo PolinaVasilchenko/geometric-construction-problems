904810b0e6fa4cf60c2ef6542137486800730374
Polina Vasilchenko <polina.vasilchenco@gmail.com>
2019-07-06 23:23:03 +1000

    add all files
    Add main.html, main.js, main.ccs and media files

---
+++ main.js
@@ -0,0 +1,167 @@
+$(document).ready(function(){
+
+  	$("#flip11").click(function(){
+    	$("#panel11").slideToggle("slow");
+  });
+
+  	$('#carousel11').carousel('pause');
+
+  	$("#flip12").click(function(){
+    	$("#panel12").slideToggle("slow");
+  });
+
+  	$('#carousel12').carousel('pause');
+
+  	$("#flip13").click(function(){
+    	$("#panel13").slideToggle("slow");
+  });
+
+  	$("#flip131").click(function(){
+    	$("#panel131").slideToggle("slow");
+  });
+
+  	$('#carousel131').carousel('pause');
+
+  	$("#flip132").click(function(){
+    	$("#panel132").slideToggle("slow");
+  });
+
+  	$('#carousel132').carousel('pause');
+
+  	$("#flip14").click(function(){
+    	$("#panel14").slideToggle("slow");
+  });
+
+  	$('#carousel14').carousel('pause');
+
+  	$("#flip15").click(function(){
+    	$("#panel15").slideToggle("slow");
+  });
+
+  	$('#carousel15').carousel('pause');
+
+  	$("#flip16").click(function(){
+    	$("#panel16").slideToggle("slow");
+  });
+
+  	$('#carousel16').carousel('pause');
+
+  	$("#flip17").click(function(){
+    	$("#panel17").slideToggle("slow");
+  });
+
+  	$('#carousel17').carousel('pause');
+
+  	$("#flip21").click(function(){
+    	$("#panel21").slideToggle("slow");
+  });
+
+  	$("#flip211").click(function(){
+    	$("#panel211").slideToggle("slow");
+  });
+
+  	$('#carousel211').carousel('pause');
+
+  	$("#flip212").click(function(){
+    	$("#panel212").slideToggle("slow");
+  });
+
+  	$('#carousel212').carousel('pause');
+
+  	$("#flip213").click(function(){
+    	$("#panel213").slideToggle("slow");
+  });
+
+  	$('#carousel213').carousel('pause');
+
+  	$("#flip22").click(function(){
+    	$("#panel22").slideToggle("slow");
+  });
+
+  	$('#carousel22').carousel('pause');
+
+  	$("#flip23").click(function(){
+    	$("#panel23").slideToggle("slow");
+  });
+
+  	$('#carousel23').carousel('pause');
+
+
+  	$("#flip24").click(function(){
+    	$("#panel24").slideToggle("slow");
+  });
+
+  	$('#carousel24').carousel('pause');
+
+  	$("#flip25").click(function(){
+    	$("#panel25").slideToggle("slow");
+  });
+
+  	$('#carousel25').carousel('pause');
+
+  	$("#flip31").click(function(){
+    	$("#panel31").slideToggle("slow");
+  });
+
+  	$('#carousel31').carousel('pause');
+
+  	$("#flip32").click(function(){
+    	$("#panel32").slideToggle("slow");
+  });
+
+  	$('#carousel32').carousel('pause');
+
+  	$("#flip33").click(function(){
+    	$("#panel33").slideToggle("slow");
+  });
+
+  	$('#carousel33').carousel('pause');
+
+  	$("#flip34").click(function(){
+    	$("#panel34").slideToggle("slow");
+  });
+
+  	$('#carousel34').carousel('pause');
+
+  	$("#flip35").click(function(){
+    	$("#panel35").slideToggle("slow");
+  });
+
+  	$('#carousel35').carousel('pause');
+
+  	$("#flip36").click(function(){
+    	$("#panel36").slideToggle("slow");
+  });
+
+  	$('#carousel36').carousel('pause');
+
+  	$("#flip37").click(function(){
+    	$("#panel37").slideToggle("slow");
+  });
+
+  	$('#carousel38').carousel('pause');
+
+  	$("#flip38").click(function(){
+    	$("#panel38").slideToggle("slow");
+  });
+
+  	$('#carousel38').carousel('pause');
+
+  	$("#flip39").click(function(){
+    	$("#panel39").slideToggle("slow");
+  });
+
+  	$('#carousel39').carousel('pause');
+
+  	$("#flip310").click(function(){
+    	$("#panel310").slideToggle("slow");
+  });
+
+  	$('#carousel310').carousel('pause');
+
+  	  	$("#flip311").click(function(){
+    	$("#panel311").slideToggle("slow");
+  });
+
+  	$('#carousel311').carousel('pause');
+});
